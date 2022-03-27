5c50502bc34a66cd7d089e4a47ce02dfe14ed9b1
shomveer kherwa <53576416+shomveerkherwa@users.noreply.github.com>
2022-03-27 22:04:01 +0530

    remove chalk dependency

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const chalk = require('chalk');
+
 const morgan = require('morgan');
 const path = require('path');
 const influencerRouter = require('./src/routers/influencerRouter');
